13af18f5285b6ebf14c6c6f57f74b958b1786b7a
Pham Thanh Luong <pluongp123@gmail.com>
2025-09-14 14:51:32 +0700

    fix show product home page and book list

--- src/main/java/com/group01/aurora_demo/catalog/controller/BookDetailServlet.java
+++ src/main/java/com/group01/aurora_demo/catalog/controller/BookDetailServlet.java
@@ -13,19 +13,19 @@ public class BookDetailServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        String idRaw = req.getParameter("id");
-        ProductDAO productDAO = new ProductDAO();
-        try {
-            long id = Long.parseLong(idRaw);
-            Product product = productDAO.getProductById(id);
-            double finalPrice = product.getPrice() - product.getDiscount();
-            int percentDiscount = (int) Math.round((product.getDiscount() / product.getPrice()) * 100);
-            req.setAttribute("product", product);
-            req.setAttribute("finalPrice", finalPrice);
-            req.setAttribute("percentDiscount", percentDiscount);
-            req.getRequestDispatcher("/WEB-INF/views/books/detail.jsp").forward(req, resp);
-        } catch (NumberFormatException e) {
-            resp.sendError(HttpServletResponse.SC_NOT_FOUND);
-        }
+        // String idRaw = req.getParameter("id");
+        // ProductDAO productDAO = new ProductDAO();
+        // try {
+        //     long id = Long.parseLong(idRaw);
+        //     Product product = productDAO.getProductById(id);
+        //     double finalPrice = product.getPrice() - product.getDiscount();
+        //     int percentDiscount = (int) Math.round((product.getDiscount() / product.getPrice()) * 100);
+        //     req.setAttribute("product", product);
+        //     req.setAttribute("finalPrice", finalPrice);
+        //     req.setAttribute("percentDiscount", percentDiscount);
+        //     req.getRequestDispatcher("/WEB-INF/views/books/detail.jsp").forward(req, resp);
+        // } catch (NumberFormatException e) {
+        //     resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+        // }
     }
 }
